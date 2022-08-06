31b6f386ced7dfa3d9ab428ccc217c1b64c55ec4
Harris <harry7801234@gmai.com>
2022-08-06 16:07:45 -0600

    hola

---
+++ pruebas.py
@@ -0,0 +1,23 @@
+print("Comandos importantes para GitHub")
+
+#-----Creará una nueva rama (-b es cuando se crea rama)
+# git checkout -b "nombre_rama" (solo agrega localmente)
+
+#-----Para subir mi codigo de repositorio local a repositorio remoto
+# git push origin nombre_rama   (origin = repo. remoto)
+
+#-----Si he creado ramas remotamente, trae las nuevas al repositorio local
+# git fetch origin
+
+#-----para cambiar entre rama ya existente
+# git checkout nombre_rama(a la que deseo moverme)
+
+
+print("para subir codigo a GitHub(cambios de código en general)")
+
+#-----Se utiliza (.) para agregar TODOS los cambios o puede agregar un archivo especifico
+# git add .
+# git add archivo_especifico.py
+
+
+print("Realizar commit")
